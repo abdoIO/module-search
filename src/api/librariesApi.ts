7b816515a8189154e9724fda99214bcd90520a82
Abdo Hafez <8437364+abdoIO@users.noreply.github.com>
2024-08-26 12:18:23 +0200

    Update librariesApi.ts

--- src/api/librariesApi.ts
+++ src/api/librariesApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'a6f28eb9b9ac4a9d07c02040577ed8d9';
+const API_KEY = 'YOUR_API_KEY';
 const API_URL = 'https://libraries.io/api/search';
 
 export const searchModules = async (query: string, page: number = 1) => {
